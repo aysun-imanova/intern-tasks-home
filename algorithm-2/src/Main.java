67b4e9bf5d1bf433cc53022edab5f409bf214f1a
aysun-imanova <63191090+aysun-imanova@users.noreply.github.com>
2023-11-10 16:42:51 +0400

    task1

--- algorithm-2/src/Main.java
+++ algorithm-2/src/Main.java
@@ -1,16 +1,7 @@
 import service.ReservationService;
 
-/**
- * 2. İstifadəçidən ilk öncə ay adını sonra isə ayın gününü daxil etməsini
- * istəyin (Misal: ay -> yanvar, gün -> 30). Əgər daxil edilən ay mövcud deyilsə, yenidən
- * daxil etməsini istəyin. Daxil edilən gün ayın günlərinin sayına uyğun deyilsə (Misal: fevral max
- * 29 gün ola bilər, 31 daxil edilibsə, yanlışdır.) günü yenidən daxil etməsini istəyin. Bütün şərtlər
- * ödənildiyi halda ekrana daxil edilən ad və günü çıxarıb "uğurla rezerv edildi" yazısı çıxsın!
- * (Misal: input -> yanvar, 24; output -> yanvar 24 tarixi üçün uğurlar rezerv edildi)
- *
- */
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         ReservationService.makeReservation();
     }
 }
